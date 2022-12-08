a8259336918a965dced423f0b619b26b3c14e1e2
VeliCKR <velicr57@gmail.com>
2022-12-08 19:27:23 +0300

    Food Order App
    This application was created via flutter. It is a food ordering application.

---
+++ Food-Ordering-App-master/lib/screens/details/components/body.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/constants.dart';
+import 'package:food_app/screens/details/components/item_image.dart';
+import 'package:food_app/screens/details/components/order_button.dart';
+import 'package:food_app/screens/details/components/title_price_rating.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemImage(
+          imgSrc: "assets/images/burger.png",
+        ),
+        Expanded(
+          child: ItemInfo(),
+        ),
+      ],
+    );
+  }
+}
+
+class ItemInfo extends StatelessWidget {
+  const ItemInfo({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      padding: EdgeInsets.all(20),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+      ),
+      child: Column(
+        children: <Widget>[
+          shopeName(name: "MacDonalds"),
+          TitlePriceRating(
+            name: "Cheese Burger",
+            numOfReviews: 24,
+            rating: 4,
+            price: 15,
+            onRatingChanged: (value) {},
+          ),
+          Text(
+            "You must try the cheese burger with its great taste. It contains meat, lettuce, tomatoes, potatoes, pickles, onions.",
+            style: TextStyle(
+              height: 1.5,
+            ),
+          ),
+          SizedBox(height: size.height * 0.1),
+          // Free space  10% of total height
+          OrderButton(
+            size: size,
+            press: () {},
+          )
+        ],
+      ),
+    );
+  }
+
+  Row shopeName({String name}) {
+    return Row(
+      children: <Widget>[
+        Icon(
+          Icons.location_on,
+          color: ksecondaryColor,
+        ),
+        SizedBox(width: 10),
+        Text(name),
+      ],
+    );
+  }
+}
